4137ae5377184fe10de1350d60e836a1b49ebcb2
Joao Victor <jsousa.1998@alunos.utfpr.edu.br>
2024-01-02 15:41:44 -0400

    filtrando data

--- meu_app.py
+++ meu_app.py
@@ -9,8 +9,16 @@ with st.container():
     st.write("Informações fornecidas pelas Hash&Co ao longo de maio")
     st.write("Quem aprender Python? [Clique aqui](https://www.hashtagtreinamentos.com/curso-python)")
 
+@st.cache_data 
+def carregar_dados():
+    tabela = pd.read_csv("resultados.csv")
+    return tabela
+
 with st.container():
     st.write("---")
+    qtde_dias=st.selectbox("Selecione o período",["7D","15D","21D","30D"])
+    num_dias = int(qtde_dias.replace("D",""))
 
-    dados = pd.read_csv("resultados.csv")
+    dados = carregar_dados()
+    dados = dados[-num_dias:]
     st.area_chart(dados,x="Data",y="Contratos")
